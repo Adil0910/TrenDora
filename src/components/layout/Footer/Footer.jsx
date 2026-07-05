3b276cce188fd6c62c4538caca49a527b082cf31
Adil0910 <aadilehtesham63@gmail.com>
2026-07-05 11:01:22 +0530

    last commit

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} AI SaaS Platform. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} TrenDora AI Platform. All rights reserved.</p>
     </footer>
   );
 };
