d801da83fd8776730d8ad9ef6f9e79622ccb2398
Adil Ehtesham <aadilehtesham63@gmail.com>
2026-07-22 12:59:09 +0530

    Add sessionChecked state to authSlice

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -5,15 +5,18 @@ const authSlice = createSlice({
   initialState: {
     currentUser: null,
     isAuthenticated: false,
+    sessionChecked: false,
   },
   reducers: {
     setUser: (state, action) => {
       state.currentUser = action.payload;
       state.isAuthenticated = true;
+      state.sessionChecked = true;
     },
     clearUser: (state) => {
       state.currentUser = null;
       state.isAuthenticated = false;
+      state.sessionChecked = true;
     },
   },
 });
